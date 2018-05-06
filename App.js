4dcb74933133548fc1a650a370f5a9aaeb13977d
Camila Farelo <mfarelo92@gmail.com>
2018-05-06 13:02:40 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}></View>
+        <View></View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  calculatorHeader: {
+    flex: 1,
+  },
+  calculatorBodu: {
+    flex: 2,
+    backgroundColor: 'red',
+  }
+});
